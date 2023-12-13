1d4911ed25e8e2595e86514558012ad143a01251
Choo <choo000407@naver.com>
2023-12-13 21:31:14 +0900

    feat(Coach): 코치 이름을 가진다

--- src/main/java/menu/model/Coach.java
+++ src/main/java/menu/model/Coach.java
@@ -1,4 +1,16 @@
 package menu.model;
 
 public class Coach {
+    private final String name;
+
+    public Coach(final String name) {
+        this.name = name;
+        validateName();
+    }
+
+    public void validateName() {
+        if (name.length() < 2 || name.length() > 4) {
+            throw new IllegalArgumentException("이름은 2~4자 이내로 입력해야 합니다.");
+        }
+    }
 }
