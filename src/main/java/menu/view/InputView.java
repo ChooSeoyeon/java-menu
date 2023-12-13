7e7e02cf62f6a7c3c43779d9741451bdfff05ed1
Choo <choo000407@naver.com>
2023-12-13 21:01:20 +0900

    feat(InputView): 코치 이름을 입력받는다

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,4 +1,20 @@
 package menu.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class InputView {
+    public List<String> readNames() {
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+        String inputNames = Console.readLine();
+        return parseInputNamesToStringList(inputNames);
+    }
+
+    private List<String> parseInputNamesToStringList(String inputNames) {
+        try {
+            return List.of(inputNames.split(","));
+        } catch (Exception e) {
+            throw new IllegalArgumentException("코치 이름은 쉼표로 구분해서 입력해야 합니다.");
+        }
+    }
 }
