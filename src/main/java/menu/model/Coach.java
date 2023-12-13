6d9c1453ed9282a9c4f38900308504776ff030df
Choo <choo000407@naver.com>
2023-12-13 21:35:08 +0900

    feat(Coach): 못 먹는 메뉴를 가진다

--- src/main/java/menu/model/Coach.java
+++ src/main/java/menu/model/Coach.java
@@ -1,7 +1,10 @@
 package menu.model;
 
+import java.util.List;
+
 public class Coach {
     private final String name;
+    private List<String> menusDontEat;
 
     public Coach(final String name) {
         this.name = name;
@@ -10,7 +13,18 @@ public class Coach {
 
     public void validateName() {
         if (name.length() < 2 || name.length() > 4) {
-            throw new IllegalArgumentException("이름은 2~4자 이내로 입력해야 합니다.");
+            throw new IllegalArgumentException("코치 이름은 2~4자 이내로 입력해야 합니다.");
+        }
+    }
+
+    public void setMenusDontEat(final List<String> menusDontEat) {
+        this.menusDontEat = menusDontEat;
+        validateMenusDontEat();
+    }
+
+    public void validateMenusDontEat() {
+        if (menusDontEat.size() > 3) {
+            throw new IllegalArgumentException("못 먹는 메뉴는 0~2개 입력해야 합니다.");
         }
     }
 }
