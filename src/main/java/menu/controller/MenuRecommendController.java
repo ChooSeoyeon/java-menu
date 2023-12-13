b198cddf25e20f81db8dfc0f7b739db61b7fc915
Choo <choo000407@naver.com>
2023-12-13 21:04:34 +0900

    feat(Controller): 에러 메시지를 관리한다

--- src/main/java/menu/controller/MenuRecommendController.java
+++ src/main/java/menu/controller/MenuRecommendController.java
@@ -1,4 +1,41 @@
 package menu.controller;
 
+import java.util.List;
+import java.util.function.Supplier;
+import menu.view.InputView;
+import menu.view.OutputView;
+
 public class MenuRecommendController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public MenuRecommendController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        List<String> coachNames = repeatUntilSuccessWithReturn(inputView::readNames);
+    }
+
+    private <T> T repeatUntilSuccessWithReturn(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private void repeatUntilSuccess(Runnable action) {
+        while (true) {
+            try {
+                action.run();
+                return;
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
 }
